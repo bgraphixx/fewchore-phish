bdd2ae34d1ed7589322e7b95be396bebba8531d4
Ebubechukwu Ibeh <ebubechukwu.ibeh@fewchorefinance.com>
2025-07-31 17:25:11 +0100

    added WhiteNoise

--- email_tracker/email_tracker/settings.py
+++ email_tracker/email_tracker/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'email_tracker.urls'
@@ -125,6 +126,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
